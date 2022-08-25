8973192c2b890119a655710e42ceb0cc3def136c
Allvaa <alvamailindo@gmail.com>
2022-08-26 00:46:08 +0700

    publish plugin

---
+++ index.jsx
@@ -0,0 +1,24 @@
+const { Plugin } = require("powercord/entities");
+const { React, getModule, getModuleByDisplayName } = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
+
+module.exports = class ShowMediaLinks extends Plugin {
+    startPlugin() {
+        const Message = getModule(x => x.default?.displayName === "Message", false);
+        const Anchor = React.memo(getModuleByDisplayName("Anchor", false));
+
+        inject("sml-inject", Message, "default", (e) => {
+            const { props } = e[0].childrenMessageContent;
+            if (props.content.length === 0 && props.message.content.length !== 0) {
+                props.content.push(
+                    <Anchor title={props.message.content} href={props.message.content}>{props.message.content}</Anchor>
+                );
+            }
+            return e;
+        }, true)
+    }
+
+    pluginWillUnload() {
+        uninject("sml-inject");
+    }
+}
